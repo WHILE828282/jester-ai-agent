8332a9b261c757370d3d58fe5fe9938dcc3bc080
LuminexisAI <ilyas.gasanov.2020@gmail.com>
2026-01-09 02:33:51 +0300

    Delete src directory

--- src/logger.ts
+++
@@ -1,6 +0,0 @@
-export type LogLevel = "INFO" | "WARN" | "ERROR" | "DEBUG";
-
-export function log(level: LogLevel, message: string, meta: any = {}) {
-  const time = new Date().toISOString();
-  console.log(JSON.stringify({ time, level, message, ...meta }));
-}
